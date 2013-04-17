0e36559c8e35fd43bae9c8a9a184a594d5811d9e
arekkas <aeneas.rekkas@serlo.org>
2013-04-17 11:26:13 +0200

    code-synch

---
+++ src/module/Entity/src/Entity/Adapter/Exercise/Text.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Entity\Adapter\Exercise;
+
+use Entity\Adapter\AbstractEntityAdapter;
+
+class Text extends AbstractEntityAdapter {
+	
+}
--- src/module/Entity/src/Entity/Service/EntityService.php
+++ src/module/Entity/src/Entity/Service/EntityService.php
@@ -15,27 +15,133 @@ use Auth\Service\AuthServiceInterface;
 use Versioning\RepositoryManagerAwareInterface;
 use Versioning\RepositoryManagerInterface;
 use Doctrine\ORM\EntityManager;
+use Core\Service\LanguageService;
+use Core\Service\SubjectService;
+use Versioning\Service\RepositoryServiceInterface;
+use Versioning\Entity\RevisionInterface;
 
 class EntityService extends AbstractEntityAdapter implements EntityServiceInterface, EventManagerAwareInterface, RepositoryManagerAwareInterface {
     
+	/**
+	 * @var AuthServiceInterface
+	 */
     protected $authService;
+
+    /**
+     * @var RepositoryManagerInterface
+     */
     protected $repositoryManager;
+    
+    /**
+     * @var EntityManager
+     */
     protected $entityManager;
+
+    /**
+     * @var EventManagerInterface
+     */
     protected $events;
+
+    /**
+     * @var LanguageService
+     */
+    protected $languageService;
     
-    protected $entityClassName;
+    /**
+     * @var SubjectService
+     */
+    protected $subjectService;
     
-    public function setEntity($entity){
-    	if(! $entity instanceOf $this->entityClassName)
+    /**
+     * @var array
+     */
+    protected $entityPrototypes = array();
+    
+    /**
+	 * @return the $languageService
+	 */
+	public function getLanguageService() {
+		return $this->languageService;
+	}
+
+	/**
+	 * @return the $subjectService
+	 */
+	public function getSubjectService() {
+		return $this->subjectService;
+	}
+
+	/**
+	 * @param LanguageService $languageService
+	 */
+	public function setLanguageService($languageService) {
+		$this->languageService = $languageService;
+		return $this;
+	}
+
+	/**
+	 * @param SubjectService $subjectService
+	 */
+	public function setSubjectService($subjectService) {
+		$this->subjectService = $subjectService;
+		return $this;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see \Core\Entity\AbstractEntityAdapter::setEntity()
+	 */
+	public function setEntity($entity){
+    	if(! $entity instanceOf $this->$classNamePrototypes['entity'])
     		throw new \Exception('Please use only entites that are an instance of: `'.$this->entityClassName.'`');
     	
     	$this->adaptee = $entity;
     	return $this;
     }
     
-    public function __construct($adaptee, $entityClassName = '\Entity\Entity\Entity'){
-    	$this->entityClassName = $entityClassName;
+    /**
+     * @return array
+     */
+    private function _defaultEntityPrototypes(){
+    	return array(
+    		'entity' => 'Entity\Entity\Entity',
+    		/*'entityType' => 'Entity\Entity\EntityType',
+    			
+    		'component' => 'Entity\Entity\Component',
+    			
+    		'comment' => 'Entity\Entity\Comment',
+    			
+    		'license' => 'Entity\Entity\License',
+    			
+    		'tag' => 'Entity\Entity\Tag',
+    			
+    		'link' => 'Entity\Entity\Link',
+    		'linkType' => 'Entity\Entity\LinkType',
+    			
+    		'repository' => 'Entity\Entity\Repository',
+    		'revision' => 'Entity\Entity\Revision',
+    		'revisionField' => 'Entity\Entity\RevisionField',
+    		'revisionValue' => 'Entity\Entity\RevisionValue',*/
+    	);
+    }
+    
+    public function __construct($adaptee, array $entityPrototypes = NULL){
+    	foreach($entityPrototypes as $name => $class)
+    		$this->setEntityPrototype($name, $class);
+    	
     	return parent::__construct($adaptee);
+    }
+    
+    public function setEntityPrototype($name, $class){
+    	if(! array_key_exists ( $name, $this->_defaultEntityPrototypes() ) )
+    		throw new \Exception('The entity prototype with key `'.$name.'` is not supported by this service.');
+    	
+    	$this->entityPrototypes[$name] = $class;
+    	return $this;
+    }
+    
+    public function getEntityPrototype($name){
+    	return $this->entityPrototypes[$name];
     }
         
 	/**
@@ -106,8 +212,8 @@ class EntityService extends AbstractEntityAdapter implements EntityServiceInterf
 		return $this;
 	}
 
-	public function receive($id) {
-		$this->setEntity($this->getEntityManager()->find($this->entityClassName, $id));
+	public function load($id) {
+		$this->setEntity($this->getEntityManager()->find($this->getEntityPrototype('entity'), $id));
 		return $this;
 	}
 	
@@ -116,4 +222,42 @@ class EntityService extends AbstractEntityAdapter implements EntityServiceInterf
 		$this->setEntity($entity);
 		$entity->populate($data);
 	}
+	
+	/**
+	 * Private method for returning the repository for this Entity.
+	 * 
+	 * @param string $field
+	 * @return RepositoryServiceInterface
+	 */
+	private function _getRepository(){
+		return $this->getRepositoryManager()->getRepository($this->_nameRepository());
+	}
+	
+	private function _setRepository(){
+		
+	}
+	
+	/**
+	 * Method for returning a revision.
+	 * Leave $id empty for the current revision.
+	 * 
+	 * @param string $id
+	 * @return RevisionInterface
+	 */
+	public function getRevision($id = NULL){
+		if($id === NULL){
+			return $this->_getRepository()->getCurrentRevision();
+		} else {
+			return $this->_getRepository()->getRevision($id);
+		}
+	}
+	
+	/**
+	 * Private method for giving the repository a unique ID
+	 * 
+	 * @return string
+	 */
+	private function _nameRepository(){
+		return 'Entity\Service\EntityService('.$this->getEntity()->getId().')';
+	}		
 }
