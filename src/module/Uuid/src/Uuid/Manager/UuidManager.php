21e0530ffde302f88fc46d917fb3f61909f75de1
arekkas <aeneas.rekkas@serlo.org>
2013-09-10 19:27:59 +0200

    Uuid tests

--- src/module/Uuid/src/Uuid/Manager/UuidManager.php
+++ src/module/Uuid/src/Uuid/Manager/UuidManager.php
@@ -16,42 +16,48 @@ use Uuid\Entity\UuidInterface;
 
 class UuidManager implements UuidManagerInterface
 {
-    use \Common\Traits\ObjectManagerAwareTrait, \Common\Traits\EntityDelegatorTrait, \Common\Traits\InstanceManagerTrait;
-    
-    public function inject(UuidHolder $entity, UuidInterface $uuid = NULL){
-        if(!$uuid){
+    use\Common\Traits\ObjectManagerAwareTrait,\Common\Traits\InstanceManagerTrait;
+
+    public function inject (UuidHolder $entity, UuidInterface $uuid = NULL)
+    {
+        if (! $uuid) {
             $uuid = $this->create();
         }
         return $entity->setUuid($uuid);
     }
-    
-    public function get($key){
-        $className = $this->resolve('manages');
-        if(is_numeric($key)){
-            $entity = $this->getObjectManager()->find($this->getClassResolver()->resolveClassName('Uuid\Entity\UuidInterface'), (int) $key);
+
+    public function get ($key)
+    {
+        if (is_numeric($key)) {
+            $entity = $this->getObjectManager()->find($this->getClassResolver()
+                ->resolveClassName('Uuid\Entity\UuidInterface'), (int) $key);
         } elseif (is_string($key)) {
-            $entity = $this->getObjectManager()->getRepository($this->getClassResolver()->resolveClassName('Uuid\Entity\UuidInterface'))->findOneByUuid((string) $key);
-        } elseif ($key instanceof $className){
+            $entity = $this->getObjectManager()
+                ->getRepository($this->getClassResolver()
+                ->resolveClassName('Uuid\Entity\UuidInterface'))
+                ->findOneByUuid((string) $key);
+        } elseif ($key instanceof \Uuid\Entity\UuidInterface) {
             $entity = $key;
         } else
-            throw new \InvalidArgumentException(); 
+            throw new \InvalidArgumentException();
         
-        if(!is_object($entity))
+        if (! is_object($entity))
             throw new \Exception('not found');
-
+        
         $this->addInstance($entity->getId(), $entity);
         return $entity;
     }
-    
-    public function factory($class){
+
+    public function factory ($class)
+    {
         return new $class($this->create());
     }
-    
-    public function create(){
+
+    public function create ()
+    {
         $entity = $this->createInstance('Uuid\Entity\UuidInterface');
-        $em = $this->getObjectManager();
-        $em->persist($entity);
-        $em->flush($entity);
+        $this->getObjectManager()->persist($entity);
+        $this->getObjectManager()->flush($entity);
         $this->addInstance($entity->getId(), $entity);
         return $entity;
     }
---
+++ src/module/Uuid/test/UuidTest/UuidManagerTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * 
+ * Athene2 - Advanced Learning Resources Manager
+ *
+ * @author	Aeneas Rekkas (aeneas.rekkas@serlo.org)
+ * @license	LGPL-3.0
+ * @license	http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
+ * @link		https://github.com/serlo-org/athene2 for the canonical source repository
+ * @copyright Copyright (c) 2013 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
+ */
+namespace UuidTest;
+
+use Uuid\Manager\UuidManager;
+use Uuid\Entity\Uuid;
+
+class UuidManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $uuidManager;
+
+    public function setUp ()
+    {
+        $sm = Bootstrap::getServiceManager();
+        $this->uuidManager = new UuidManager();
+        
+        $this->uuidManager->setClassResolver($sm->get('ClassResolver\ClassResolver'));
+        $this->uuidManager->setObjectManager($this->getEmMock());
+        $this->uuidManager->setServiceLocator($sm);
+    }
+
+    public function testGet ()
+    {
+        $this->uuidManager->getObjectManager()
+            ->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue((new Uuid())->setId(1)));
+        
+        $entity = $this->uuidManager->get(1);
+        $this->assertNotNull($entity);
+        $this->assertEquals(1, $entity->getId());
+    }
+
+    public function testCreate ()
+    {
+        $uuid = $this->uuidManager->create();
+        $this->assertNotNull($uuid);
+        $this->assertInstanceOf('\Uuid\Entity\UuidInterface', $uuid);
+    }
+
+    protected function getEmMock ()
+    {
+        $emMock = $this->getMock('\Doctrine\ORM\EntityManager', array(
+            'getRepository',
+            'getClassMetadata',
+            'persist',
+            'flush',
+            'find'
+        ), array(), '', false);
+        $emMock->expects($this->any())
+            ->method('getClassMetadata')
+            ->will($this->returnValue((object) array(
+            'name' => 'aClass'
+        )));
+        $emMock->expects($this->any())
+            ->method('persist')
+            ->will($this->returnValue(null));
+        $emMock->expects($this->any())
+            ->method('flush')
+            ->will($this->returnValue(null));
+        return $emMock; // it
+                        // tooks
+                        // 13
+                        // lines
+                        // to
+                        // achieve
+                        // mock!
+    }
+}
