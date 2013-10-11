2eedc6db7aeb71019c4202efb71b47d8474f0ac7
Ju <ju@formundcode.de>
2013-10-11 17:15:27 +0200

    update build

--- src/module/Ui/assets/build/scripts/main.js
+++ src/module/Ui/assets/build/scripts/main.js
@@ -3671,15 +3671,79 @@ define('referrer_history',["underscore", "cache"], function (_, cache) {
  * Saves the browser pathname on the client side.
  */
 
-/*global define*/
-define("side_navigation", ["jquery", "referrer_history"], function ($, ReferrerHistory) {
+/*global define, console*/
+define("side_navigation", ["jquery", "underscore", "referrer_history"], function ($, _, ReferrerHistory, undefined) {
     
     var defaults,
         instance,
+        Hierarchy,
         Plugin;
 
     defaults = {
-        mainId: '#serlo-nav'
+        mainId: '#main-nav',
+        activeClass: 'is-active'
+    };
+
+    Hierarchy = function () {
+        this.data = [];
+    };
+
+    Hierarchy.prototype.fetchFromDom = function ($root) {
+        var self = this,
+            deepness = [];
+
+        self.data = [];
+        self.$root = $root;
+
+        function loop($element, dataHierarchy, level) {
+
+            $('> li', $element).each(function (i) {
+                deepness.splice(level);
+                deepness.push(i);
+
+                var $listItem = $(this),
+                    $link = $listItem.children().filter('a').first();
+
+                dataHierarchy[i] = {
+                    url: $link.attr('href'),
+                    title: $link.text().trim(),
+                    $li: $listItem,
+                    $a: $link,
+                    position: [].concat(deepness)
+                };
+
+                if ($listItem.children().filter('ul').length) {
+                    dataHierarchy[i].children = [];
+                    loop($listItem.children().filter('ul').first(), dataHierarchy[i].children, level + 1);
+                }
+            });
+        }
+
+        loop(self.$root, self.data, 0);
+        return this;
+    };
+
+    Hierarchy.prototype.findByUrl = function (url) {
+        var self = this;
+
+        function deepFlatten(item) {
+            if (item.children) {
+                return [].concat(item, deepFlatten(item.children));
+            }
+            return item;
+        }
+
+        function find(dataHierarchy) {
+            return _.chain(dataHierarchy).map(deepFlatten).flatten().filter(function (item) {
+                if (item.url === url) {
+                    console.log('FOUND IT', item);
+                    return item;
+                }
+                return false;
+            }).value();
+        }
+
+        return _.first(find(self.data));
     };
 
     Plugin = function (options) {
@@ -3692,11 +3756,26 @@ define("side_navigation", ["jquery", "referrer_history"], function ($, ReferrerH
         this.$el = $(this.options.mainId);
         this.$allLinks = $('li > a', this.$el);
 
-        this.$activeLink = this.findActiveByUrl(ReferrerHistory.getOne());
+        this.hierarchy = new Hierarchy();
+        this.hierarchy.fetchFromDom(this.$el);
+
+        this.active = this.hierarchy.findByUrl(ReferrerHistory.getOne());
+
+        this.setActiveBranch();
     };
 
     Plugin.prototype.findActiveByUrl = function (url) {
         return this.$allLinks.filter('[href=\"' + url + '\"]').first();
+    };
+
+    Plugin.prototype.setActiveBranch = function () {
+        if (this.active) {
+            this.active.$li
+                .addClass(this.options.activeClass)
+                .parents('li')
+                .addClass(this.options.activeClass);
+        }
+        return this;
     };
 
     return function (options) {
