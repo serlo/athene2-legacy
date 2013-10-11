436a656061580be37a8bf8a19f3869d7357884ef
Ju <ju@formundcode.de>
2013-10-11 14:30:07 +0200

    updated build

--- src/module/Ui/assets/build/scripts/main.js
+++ src/module/Ui/assets/build/scripts/main.js
@@ -3667,15 +3667,63 @@ define('referrer_history',["underscore", "cache"], function (_, cache) {
  * @license http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
  * @link        https://github.com/serlo-org/athene2 for the canonical source repository
  * @copyright Copyright (c) 2013 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
+ * 
+ * Saves the browser pathname on the client side.
+ */
+
+/*global define*/
+define("side_navigation", ["jquery", "referrer_history"], function ($, ReferrerHistory) {
+    
+    var defaults,
+        instance,
+        Plugin;
+
+    defaults = {
+        mainId: '#serlo-nav'
+    };
+
+    Plugin = function (options) {
+        if (!(this instanceof Plugin)) {
+            return new Plugin(options);
+        }
+
+        this.options = options ? $.extend({}, defaults, options) : $.extend({}, defaults);
+
+        this.$el = $(this.options.mainId);
+        this.$allLinks = $('li > a', this.$el);
+
+        this.$activeLink = this.findActiveByUrl(ReferrerHistory.getOne());
+    };
+
+    Plugin.prototype.findActiveByUrl = function (url) {
+        return this.$allLinks.filter('[href=\"' + url + '\"]').first();
+    };
+
+    return function (options) {
+        // singleton
+        return instance || (function () {
+            instance = new Plugin(options);
+            return instance;
+        }());
+    };
+});
+/**
+ * 
+ * Athene2 - Advanced Learning Resources Manager
+ *
+ * @author  Julian Kempff (julian.kempff@serlo.org)
+ * @license LGPL-3.0
+ * @license http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
+ * @link        https://github.com/serlo-org/athene2 for the canonical source repository
+ * @copyright Copyright (c) 2013 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
  */
 /*global define, require*/
-define("ATHENE2", ['jquery', 'referrer_history'], function ($, ReferrerHistory) {
+define("ATHENE2", ['jquery', 'side_navigation'], function ($, SideNavigation) {
     
 
     function init() {
-        // voidvoid
-        $.extend({});
-        ReferrerHistory.getOne();
+        $.extend();
+        new SideNavigation();
     }
 
     return {
