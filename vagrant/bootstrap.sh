affa97403afba011efb30f99f1ef55f15cc955a1
arekkas <aeneas.rekkas@serlo.org>
2014-01-16 17:25:15 +0100

    Increased composer timeout

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -82,8 +82,8 @@ sudo su - www-data -c "cd /var/www/src/module/Ui/assets && bower update"
 sudo su - www-data -c "cd /var/www/src/module/Ui/assets && grunt build"
 sudo su - www-data -c "pm2 start /var/www/src/module/Ui/assets/node_modules/athene2-editor/server/server.js"
 sudo su - www-data -c "cd /var/www/ && php composer.phar self-update"
-sudo su - www-data -c "cd /var/www/ && COMPOSER_PROCESS_TIMEOUT=900 php composer.phar install"
-sudo su - www-data -c "cd /var/www/ && COMPOSER_PROCESS_TIMEOUT=900 php composer.phar update"
+sudo su - www-data -c "cd /var/www/ && COMPOSER_PROCESS_TIMEOUT=2400 php composer.phar install"
+sudo su - www-data -c "cd /var/www/ && COMPOSER_PROCESS_TIMEOUT=2400 php composer.phar update"
 ' >> /home/vagrant/startup.sh
 
 echo '
