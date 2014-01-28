38036239ee7e7102839256558b2c7b3854d753e7
arekkas <aeneas.rekkas@serlo.org>
2014-01-28 21:42:49 +0100

    Y U NO WORK DEPLOY

--- src/public/deploy.php
+++ src/public/deploy.php
@@ -1,49 +1,3 @@
-<?php
-set_time_limit(0);
-
-/**
- * GIT DEPLOYMENT SCRIPT
- *
- * Used for automatically deploying websites via github or bitbucket, more deets here:
- *
- *        https://gist.github.com/1809044
- */
-
-// The commands
-$commands = array(
-    'echo $PWD',
-    'whoami',
-    'git pull --ff',
-    'git status',
-    'git submodule sync',
-    'git submodule update',
-    'git submodule status',
-    'sh ' . __DIR__ . '/../hyperdrive.sh',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 dump',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && npm clean',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && npm install',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && npm update',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && bower clean',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && bower install',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && bower update',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && grunt build --force',
-    'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 start node_modules/athene2-editor/server/server.js',
-    'cd ' . __DIR__ . '/../../ && php composer.phar self-update',
-    'cd ' . __DIR__ . '/../../ && php composer.phar update',
-);
-
-// Run the commands for output
-$output = '';
-foreach ($commands AS $command) {
-    // Run it
-    $tmp = shell_exec($command);
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-}
-
-// Make it pretty for manual user access (and why not?)
-?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
@@ -57,8 +11,52 @@ foreach ($commands AS $command) {
 [| <span style="color: #FF0000;">&hearts; &hearts;</span> |]  | Git Deployment Script v0.1 |
  |___==___|  / &copy; oodavid 2012 |
               |____________________________|
+    <?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via github or bitbucket, more deets here:
+     *
+     *        https://gist.github.com/1809044
+     */
 
-    <?php echo $output; ?>
+    // The commands
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull --ff',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+        'sh ' . __DIR__ . '/../hyperdrive.sh',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 dump',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && npm clean',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && npm install',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && npm update',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && bower clean',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && bower install',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && bower update',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && grunt build',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 start node_modules/athene2-editor/server/server.js',
+        'cd ' . __DIR__ . '/../../ && php composer.phar self-update',
+        'cd ' . __DIR__ . '/../../ && php composer.phar update',
+    );
+
+    // Run the commands for output
+    $output = '';
+    foreach ($commands AS $command) {
+        // Run it
+        $tmp = shell_exec($command);
+        // Output
+        $output = "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+        $output .= htmlentities(trim($tmp)) . "\n";
+        echo $output;
+        flush();
+    }
+
+    // Make it pretty for manual user access (and why not?)
+    ?>
 </pre>
 </body>
 </html>
