3cb974f53b4864c620442c31d8df55548f9c890e
arekkas <aeneas.rekkas@serlo.org>
2014-01-28 21:43:58 +0100

    Maybe like this (deploy.php)?

--- src/public/deploy.php
+++ src/public/deploy.php
@@ -34,12 +34,12 @@
         'cd ' . __DIR__ . '/../module/Ui/assets/ && npm clean',
         'cd ' . __DIR__ . '/../module/Ui/assets/ && npm install',
         'cd ' . __DIR__ . '/../module/Ui/assets/ && npm update',
-        'cd ' . __DIR__ . '/../module/Ui/assets/ && bower clean',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 start node_modules/athene2-editor/server/server.js',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && bower cache clean',
         'cd ' . __DIR__ . '/../module/Ui/assets/ && bower install',
         'cd ' . __DIR__ . '/../module/Ui/assets/ && bower update',
-        'cd ' . __DIR__ . '/../module/Ui/assets/ && grunt build',
-        'cd ' . __DIR__ . '/../module/Ui/assets/ && pm2 start node_modules/athene2-editor/server/server.js',
         'cd ' . __DIR__ . '/../../ && php composer.phar self-update',
+        'cd ' . __DIR__ . '/../module/Ui/assets/ && grunt build',
         'cd ' . __DIR__ . '/../../ && php composer.phar update',
     );
 
