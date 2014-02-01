3239ee25b45ec05416dc8f39ec184673bb7f4aa9
arekkas <aeneas.rekkas@serlo.org>
2014-02-01 20:06:39 +0100

    Dirfix

--- src/public/deploy.php
+++ src/public/deploy.php
@@ -1,5 +1,5 @@
-<?php set_time_limit(0); ?>
-<?php putenv('HOME=' . __DIR__ . '../../'); ?>
+<?php set_time_limit(1200); ?>
+<?php putenv('HOME=' . __DIR__ . '/../../'); ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
@@ -27,16 +27,16 @@
         "git pull --ff 2>&1",
         "git status 2>&1",
         "cd " . __DIR__ . "/../;sh hyperdrive.sh 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm install 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm update 2>&1",
-        "pm2 dump && pm2 kill 2>&1",
-        "pm2 start \"" . __DIR__ . "/../module/Ui/assets/node_modules/athene2-editor/server/server.js\" 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower install 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower update 2>&1",
-        "cd " . __DIR__ . "/../module/Ui/assets/;grunt build 2>&1",
-        "cd " . __DIR__ . "/../../;php composer.phar update 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm install",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm update",
+        "pm2 dump && pm2 kill",
+        "pm2 start \"" . __DIR__ . "/../module/Ui/assets/node_modules/athene2-editor/server/server.js\"",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower install",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower update",
+        "cd " . __DIR__ . "/../module/Ui/assets/;grunt build",
+        "cd " . __DIR__ . "/../../;php composer.phar update",
     );
 
     // Run the commands for output
