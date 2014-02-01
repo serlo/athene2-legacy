9af0751e25690e672ad116d729a705ff9f7a6fdf
arekkas <aeneas.rekkas@serlo.org>
2014-02-01 17:57:04 +0100

    is deployment finally fixed? #121 #122

--- src/public/deploy.php
+++ src/public/deploy.php
@@ -1,4 +1,5 @@
 <?php set_time_limit(0); ?>
+<?php putenv(__DIR__.'../../'); ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
@@ -23,25 +24,24 @@
 
     // The commands
     $commands = array(
-        "echo $PWD",
-        "whoami",
-        "git pull --ff",
-        "git status",
-        "git submodule sync",
-        "git submodule update",
-        "git submodule status",
-        "sh " . __DIR__ . "/../hyperdrive.sh",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm install",
-        "cd " . __DIR__ . "/../module/Ui/assets/;npm update",
-        "cd " . __DIR__ . "/../module/Ui/assets/;pm2 dump && pm2 kill",
-        "pm2 start ".__DIR__."/../module/Ui/assets/node_modules/athene2-editor/server/server.js",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower install",
-        "cd " . __DIR__ . "/../module/Ui/assets/;bower update",
-        "cd " . __DIR__ . "/../module/Ui/assets/;grunt build",
-        "cd " . __DIR__ . "/../../;php-cli composer.phar self-update",
-        "cd " . __DIR__ . "/../../;php-cli composer.phar update",
+        "echo $PWD 2>&1",
+        "whoami 2>&1",
+        "git pull --ff 2>&1",
+        "git status 2>&1",
+        "git submodule sync 2>&1",
+        "git submodule update 2>&1",
+        "git submodule status 2>&1",
+        "sh " . __DIR__ . "/../hyperdrive.sh 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm install 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm update 2>&1",
+        "pm2 dump && pm2 kill 2>&1",
+        "pm2 start \"".__DIR__."/../module/Ui/assets/node_modules/athene2-editor/server/server.js\" 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower install 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower update 2>&1",
+        "cd " . __DIR__ . "/../module/Ui/assets/;grunt build 2>&1",
+        "cd " . __DIR__ . "/../../;php-cli composer.phar update 2>&1",
     );
 
     // Run the commands for output
