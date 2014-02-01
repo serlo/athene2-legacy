d35d17e13b75ba77fbeff1ccc88cecc4c5b5383c
arekkas <aeneas.rekkas@serlo.org>
2014-02-01 17:19:28 +0100

    deploy pls #121 #122

--- src/public/deploy.php
+++ src/public/deploy.php
@@ -1,52 +1,4 @@
-<<<<<<< HEAD
-=======
-<?php
-set_time_limit(0);
-
-/**
- * GIT DEPLOYMENT SCRIPT
- *
- * Used for automatically deploying websites via github or bitbucket, more deets here:
- *
- *        https://gist.github.com/1809044
- */
-
-// The commands
-$commands = array(
-    "echo $PWD",
-    "whoami",
-    "git pull --ff",
-    "git status",
-    "git submodule sync",
-    "git submodule update",
-    "git submodule status",
-    "sh " . __DIR__ . "/../hyperdrive.sh",
-    "cd " . __DIR__ . "/../module/Ui/assets/;pm2 dump && pm2 kill",
-    "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean",
-    "cd " . __DIR__ . "/../module/Ui/assets/;npm install",
-    "cd " . __DIR__ . "/../module/Ui/assets/;npm update",
-    "pm2 start ".__DIR__."/../module/Ui/assets/node_modules/athene2-editor/server/server.js",
-    "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean",
-    "cd " . __DIR__ . "/../module/Ui/assets/;bower install",
-    "cd " . __DIR__ . "/../module/Ui/assets/;bower update",
-    "cd " . __DIR__ . "/../module/Ui/assets/;grunt build",
-    "cd " . __DIR__ . "/../../;php-cli composer.phar self-update",
-    "cd " . __DIR__ . "/../../;php-cli composer.phar update",
-);
-
-// Run the commands for output
-$output = '';
-foreach ($commands AS $command) {
-    // Run it
-    $tmp = shell_exec($command);
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-}
-
-// Make it pretty for manual user access (and why not?)
-?>
->>>>>>> develop
+<?php set_time_limit(0); ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
@@ -71,27 +23,25 @@ foreach ($commands AS $command) {
 
     // The commands
     $commands = array(
-        'echo $PWD',
-        'whoami',
-        'compass -v',
-        'sass -v',
-        'git pull --ff',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;pm2 stop server.js)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;npm cache clean)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;npm install)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;npm update)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;pm2 start node_modules/athene2-editor/server/server.js)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;bower cache clean)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;bower install)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;bower update)',
-        '(cd ' . __DIR__ . '/../;sh hyperdrive.sh)',
-        '(cd ' . __DIR__ . '/../module/Ui/assets/;grunt build --debug --stack --verbose)',
-        '(cd ' . __DIR__ . '/../../;php-cli composer.phar self-update)',
-        '(cd ' . __DIR__ . '/../../;php-cli composer.phar update)',
+        "echo $PWD",
+        "whoami",
+        "git pull --ff",
+        "git status",
+        "git submodule sync",
+        "git submodule update",
+        "git submodule status",
+        "sh " . __DIR__ . "/../hyperdrive.sh",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm cache clean",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm install",
+        "cd " . __DIR__ . "/../module/Ui/assets/;npm update",
+        "cd " . __DIR__ . "/../module/Ui/assets/;pm2 dump && pm2 kill",
+        "pm2 start ".__DIR__."/../module/Ui/assets/node_modules/athene2-editor/server/server.js",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower cache clean",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower install",
+        "cd " . __DIR__ . "/../module/Ui/assets/;bower update",
+        "cd " . __DIR__ . "/../module/Ui/assets/;grunt build",
+        "cd " . __DIR__ . "/../../;php-cli composer.phar self-update",
+        "cd " . __DIR__ . "/../../;php-cli composer.phar update",
     );
 
     // Run the commands for output
