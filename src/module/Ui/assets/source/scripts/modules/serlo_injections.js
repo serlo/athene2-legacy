300170487ff11a104fa6536b9dc89cbc35915546
Ju <ju@formundcode.de>
2014-02-07 15:13:26 +0100

    finally show multiple geogebra applets

--- src/module/Ui/assets/source/scripts/modules/serlo_injections.js
+++ src/module/Ui/assets/source/scripts/modules/serlo_injections.js
@@ -9,14 +9,15 @@
  * @copyright Copyright (c) 2013 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
  */
 
-/*global define, require, window, web*/
+/*global define, require, window*/
 define(['jquery', 'common', 'translator'], function ($, Common, t) {
     "use strict";
     var Injections,
         cache = {},
-        ggbApplets = [],
+        ggbApplets = {},
+        ggbAppletsCount = 0,
         geogebraScriptSource = 'http://www.geogebra.org/web/4.4/web/web.nocache.js',
-        $geogebraTemplate = $('<article class="geogebraweb" data-param-width="500" data-param-height="500" data-param-usebrowserforjs="true" data-param-enableRightClick="true"></article>');
+        $geogebraTemplate = $('<article class="geogebraweb" data-param-width="500" data-param-height="500" data-param-usebrowserforjs="true" data-param-enableRightClick="false"></article>');
 
     // terrible geogebra oninit handler..
     // that doesnt work.....
@@ -27,6 +28,8 @@ define(['jquery', 'common', 'translator'], function ($, Common, t) {
     };
 
     Injections = function () {
+        var totalInjectionsCount = $(this).length;
+
         return $(this).each(function () {
             var $that = $(this),
                 $a = $('> a', $that),
@@ -38,15 +41,21 @@ define(['jquery', 'common', 'translator'], function ($, Common, t) {
             }
 
             function initGeogebraApplet(xml) {
-                var ggbAppletID = ggbApplets.length,
+                var ggbAppletID = 'ggbApplet' + ggbAppletsCount,
                     $clone = $geogebraTemplate.clone();
 
-                $clone.attr("data-param-id", ggbAppletID);
-                $clone.attr("data-param-ggbbase64", btoa(xml));
+                ggbAppletsCount += 1;
+
+                $clone.attr('data-param-id', ggbAppletID);
+                // initialize geogebra applet with an empty dummy document
+                // otherwise it wont initialize and we will never be able
+                // to call the setXML method..
+                $clone.attr("data-param-ggbbase64", "UEsDBBQACAgIAD10R0QAAAAAAAAAAAAAAAAWAAAAZ2VvZ2VicmFfamF2YXNjcmlwdC5qc0srzUsuyczPU0hPT/LP88zLLNHQVKiuBQBQSwcI1je9uRkAAAAXAAAAUEsDBBQACAgIAD10R0QAAAAAAAAAAAAAAAAMAAAAZ2VvZ2VicmEueG1srVZtb9s2EP6c/oqDPqc2qTfbgZyiLVCgQBYUSzcU/UZJtMxZFgWRsp2iP353pGQ7Lx2WZXFk6njPvfLu6OzdYVvDTnZG6WYZ8AkLQDaFLlVTLYPert7Og3fXb7JK6krmnYCV7rbCLoN4EgcnOaQmnJEwHIy6avSt2ErTikLeFWu5FTe6ENYh19a2V9Ppfr+fjDonuqumVZVPDqYMAP1pzDIYXq5Q3QOhfeTgIWN8+u23G6/+rWqMFU0hAyBfe3X95iLbq6bUe9ir0q6XQZKmAaylqtbk/GIewJRALUbQysKqnTQoekaCKpeB3baBg4mG+Bf+DepjOAGUaqdK2S2DW3EbgO6UbOzA5IOR6Sie7ZTcez305kzEbDHDTCqj8loug5WoDYahmlWHKUQPuh5JY+9rmYtupE8O8Ev8IED9kKQLT8DHjRzGLumZ4ZMkzPtybjgAq3XttLJ/8GCgTy4MGw98GD2InvMgxce59siD+ZkHiOPwE3AJ/RIB/HQviafjgUw9OXMLZ37hA3NOXwsi0ldGFP2niPiZVX9SLzF6PMbFC0yGrzF5jPIZg2HyixhfmdpnE4u23L97npiMwpeYfNIi/9ZiNh37MxvqEsyasENerdziZHIlBwmWazqDS0iAL3CZUdmGwBOIEyT5HFJaZxBRpcYQwRwIxyOIYxKb41fsqjiFBHXR5syXM0QxJBFgVV9CGAOEDMKQ3jmEESKSBBIUIuuczEYpxCkS0RziBXpGmqiZIpRDGo2HEHGISJbPIEwhDWEWkXhMXieQEjxmEHOInSVEzSGiCPC0W22UTyWjGVq3x6S7rKmm7e2QqWG/2JZj1qx+BC91sflwTO3AkcLYcxjOzNMo9jP0waS+yGqRyxpvqTs6d4CdqKmcnYWVbiwMZ85Dv1d1ol2rwtxJa1HKwF9iJ26ElYdPiDajbWfaXSCZ7ItalUo0f2JRkApSCON94nr0eJ/wwXKhdVfe3RusFDh8l53G/uTJhJ3+4gSn3r1nRQ9ZKd4DphBU4wk7Z3BGl9f9r3nOttwdYxMHacb8Vx110JBZIj6bD7o+bbVaNfajaG3fuTsfR0JHUb1vqlq65LoOwnu22OT6cOeyGqZe19f71g0P50BefdS17gBbMEwSBAxr7leHIc+OKOYwzCHYeEyqPPL5InQIt+Z+dSg8d+/aECkfw+RsNKOMmxyo/LyFXdHQrd03yt6MhFXF5hQp4W/7bY71Nog9VMn/J5XZ9FGFZRvZNbL2ddTgSfa6N76wj8V5kfVGfhF2/b4pf5cVNuUXQWPQomoPPXlcykJtUdDvD6kTdKx/oKt+t5RVJ8cIa/dzzCfWcdl5VT/Zdqo+dXr7udl9xZp55Go2HePJTNGplkoTchzLG3mqvlIZgVO9PJfD4A1GUdDIwURaSmIAordr3blfXNi1uJKFc6jr3OEn5fXfUEsHCAggsi84BAAA2AoAAFBLAQIUABQACAgIAD10R0TWN725GQAAABcAAAAWAAAAAAAAAAAAAAAAAAAAAABnZW9nZWJyYV9qYXZhc2NyaXB0LmpzUEsBAhQAFAAICAgAPXRHRAggsi84BAAA2AoAAAwAAAAAAAAAAAAAAAAAXQAAAGdlb2dlYnJhLnhtbFBLBQYAAAAAAgACAH4AAADPBAAAAAA=");
 
                 // the following doesnt work.
                 ggbApplets[ggbAppletID] = function () {
-                    // console.log('hier', this, arguments, xml);
+                    var applet = window[ggbAppletID];
+                    applet.setXML(xml);
                 };
 
                 $that.html($clone);
@@ -67,15 +76,7 @@ define(['jquery', 'common', 'translator'], function ($, Common, t) {
 
                 // check if it is geogebra xml
                 if (data.documentElement && data.documentElement.nodeName === 'geogebra') {
-                    if (typeof web === "undefined") {
-                        require([geogebraScriptSource], function () {
-                            console.log('geogebra loaded');
-
-                            initGeogebraApplet(data.documentElement.outerHTML);
-                        });
-                    } else {
-                        initGeogebraApplet(data.documentElement.outerHTML);
-                    }
+                    initGeogebraApplet(data.documentElement.outerHTML);
                 } else if (contentType === 'image/jpeg' || contentType === 'image/png') {
                     $that.html('<img src="' + href + '" title="' + title + '" />');
                 } else {
@@ -90,39 +91,6 @@ define(['jquery', 'common', 'translator'], function ($, Common, t) {
                         notSupportedYet($that);
                     }
                 }
-                // /// if response contains html,
-                // /// use that.
-                // if (data.response) {
-                //     $that.html(data.response);
-                // } else {
-                //     /// if we have a geogebra injection
-                //     if (data.type === 'geogebra') {
-                //         /// check if there is only one file
-                //         if (data.files && data.files.length > 1) {
-                //             /// wich means the applet needs to
-                //             /// be loaded.
-                //             if (typeof "web" === "undefined") {
-                //                 require([geogebraScriptSource], function () {
-                //                     initGeogebraApplet(xml);
-                //                 });
-                //             }
-                //             initGeogebraApplet(xml);
-                //         } else {
-                //             /// or simply insert the second
-                //             /// file as an image
-                //             $that.html($('<img>').attr({
-                //                 title: title,
-                //                 src: data.files[1].location
-                //             }));
-                //         }
-                //     } else if (data.files) {
-                //         $that.html($('<a>').attr({
-                //             href: data.files[0].location
-                //         }));
-                //     } else {
-                //         Common.genericError('Unknown injection: ' + href);
-                //     }
-                // }
             }
 
             if (cache[href]) {
@@ -132,6 +100,16 @@ define(['jquery', 'common', 'translator'], function ($, Common, t) {
             $.ajax(href)
                 .success(function () {
                     handleResponse(arguments[0], arguments[2].getResponseHeader('Content-Type'));
+                })
+                .always(function () {
+                    totalInjectionsCount -= 1;
+                    if (totalInjectionsCount === 0 && ggbAppletsCount > 0) {
+                        // if all injections have been loaded,
+                        // load the geogebra script and there
+                        // have been some gegeogebra injections
+                        // load the geogegebra script
+                        require([geogebraScriptSource]);
+                    }
                 })
                 .error(function () {
                     Common.log('Could not load injection');
