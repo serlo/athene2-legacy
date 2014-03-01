b2c25c0264c260920763407b6630db543805fc56
arekkas <aeneas.rekkas@serlo.org>
2014-03-01 20:37:10 +0100

    Increased vagrant timeouts and fixed minor issues

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -92,9 +92,6 @@ a2ensite athene2
 # Restart apache
 service apache2 restart
 
-# Remove automatically generated index.html
-rm /var/www/index.html
-
 # Mysql
 sudo sed -i "s/bind-address.*=.*/bind-address=0.0.0.0/" /etc/mysql/my.cnf
 mysql -u root -proot mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;"
