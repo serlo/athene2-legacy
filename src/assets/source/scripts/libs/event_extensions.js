f741c587ce3c5e38a61ca512517e13f48a5dd02f
arekkas <aeneas.rekkas@serlo.org>
2014-03-30 18:36:56 +0200

    Improved event identifiers

--- src/assets/source/scripts/libs/event_extensions.js
+++ src/assets/source/scripts/libs/event_extensions.js
@@ -3,8 +3,9 @@
     $.each(['show', 'hide', 'toggle', 'slideToggle', 'fadeIn', 'slideDown', 'animate'], function (i, ev) {
         var el = $.fn[ev];
         $.fn[ev] = function () {
+            this.trigger(ev + '.before');
             var ret = el.apply(this, arguments);
-            this.trigger(ev);
+            this.trigger(ev + '.after');
             return ret;
         };
     });
