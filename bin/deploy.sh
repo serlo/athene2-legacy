be9be2816144a071b57acbede011606e0ae19434
aeneasr <aeneas.rekkas@serlo.org>
2014-05-29 21:14:41 +0200

    Better 500 error message

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,7 +8,7 @@ pm2 start node_modules/athene2-editor/server/server.js
 bower update
 grunt build
 cd ../../
-php composer.phar update
 cd  src && php public/index.php assetic build
-pm2 status
 rm data/* -Rf
+pm2 status
+php composer.phar update
