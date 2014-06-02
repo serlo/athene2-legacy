d538958460d5896a05736fb7db700826d6f0c16b
arekkas <aeneas.rekkas@serlo.org>
2014-06-02 14:33:58 +0200

    Deploy update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,7 +8,8 @@ pm2 start node_modules/athene2-editor/server/server.js
 bower update
 grunt build
 cd ../../
-cd  src && php public/index.php assetic build
+cd src && php public/index.php assetic build
 rm data/twig data/zfc* -Rf
 pm2 status
+cd ../
 php composer.phar update
