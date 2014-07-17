852abafcc9e4119dad91bf2371e8d3b9a6818f00
aeneasr <aeneas.rekkas@serlo.org>
2014-07-17 12:11:40 +0200

    Closes #636

--- src/assets/source/scripts/main.js
+++ src/assets/source/scripts/main.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * Athene2 - Advanced Learning Resources Manager
  *
  * @author  Julian Kempff (julian.kempff@serlo.org)
@@ -13,40 +13,41 @@ require.config({
     baseUrl: "/assets/build/scripts",
     paths: {
         "jquery": "../bower_components/jquery/jquery",
-        "jquery-ui" : "../bower_components/jquery-ui/ui/jquery-ui",
+        "jquery-ui": "../bower_components/jquery-ui/ui/jquery-ui",
         "underscore": "../bower_components/underscore/underscore",
         "bootstrap": "../bower_components/sass-bootstrap/dist/js/bootstrap",
-        "moment" : "../bower_components/momentjs/min/moment.min",
+        "moment": "../bower_components/momentjs/min/moment.min",
         "moment_de": "../bower_components/momentjs/lang/de",
-        "common" : "modules/serlo_common",
-        "easing" : "libs/easing",
+        "jasny": "../bower_components/jasny-bootstrap/dist/js/jasny-bootstrap",
+        "common": "modules/serlo_common",
+        "easing": "libs/easing",
         "events": "libs/eventscope",
         "cache": "libs/cache",
         "polyfills": "libs/polyfills",
         "event_extensions": "libs/event_extensions",
-        "referrer_history" : "modules/serlo_referrer_history",
-        "side_navigation" : "modules/serlo_side_navigation",
+        "referrer_history": "modules/serlo_referrer_history",
+        "side_navigation": "modules/serlo_side_navigation",
         "ajax_overlay": "modules/serlo_ajax_overlay",
-        "sortable_list" : "modules/serlo_sortable_list",
-        "timeago" : "modules/serlo_timeago",
-        "trigger" : "modules/serlo_trigger",
-        "system_notification" : "modules/serlo_system_notification",
-        "nestable" : "thirdparty/jquery.nestable",
-        "datepicker" : "../bower_components/bootstrap-datepicker/js/bootstrap-datepicker",
-        "translator" : "modules/serlo_translator",
-        "i18n" : "modules/serlo_i18n",
-        "side_element" : "modules/serlo_side_element",
-        "content" : "modules/serlo_content",
-        "spoiler" : "modules/serlo_spoiler",
-        "injections" : "modules/serlo_injections",
-        "search" : "modules/serlo_search",
-        "support" : "modules/serlo_supporter",
-        "modals" : "modules/serlo_modals",
-        "router" : "modules/serlo_router",
-        "forum_select" : "modules/serlo_forum_select",
-        "toggle_action" : "modules/serlo_toggle",
-        "mathjax_trigger" : "modules/serlo_mathjax_trigger",
-        "affix" : "modules/serlo_affix"
+        "sortable_list": "modules/serlo_sortable_list",
+        "timeago": "modules/serlo_timeago",
+        "trigger": "modules/serlo_trigger",
+        "system_notification": "modules/serlo_system_notification",
+        "nestable": "thirdparty/jquery.nestable",
+        "datepicker": "../bower_components/bootstrap-datepicker/js/bootstrap-datepicker",
+        "translator": "modules/serlo_translator",
+        "i18n": "modules/serlo_i18n",
+        "side_element": "modules/serlo_side_element",
+        "content": "modules/serlo_content",
+        "spoiler": "modules/serlo_spoiler",
+        "injections": "modules/serlo_injections",
+        "search": "modules/serlo_search",
+        "support": "modules/serlo_supporter",
+        "modals": "modules/serlo_modals",
+        "router": "modules/serlo_router",
+        "forum_select": "modules/serlo_forum_select",
+        "toggle_action": "modules/serlo_toggle",
+        "mathjax_trigger": "modules/serlo_mathjax_trigger",
+        "affix": "modules/serlo_affix"
     },
     shim: {
         underscore: {
@@ -65,7 +66,7 @@ require.config({
             deps: ['jquery']
         },
         ATHENE2: {
-            deps: ['bootstrap', 'easing', 'nestable', 'polyfills', 'datepicker', 'event_extensions']
+            deps: ['bootstrap', 'easing', 'nestable', 'polyfills', 'datepicker', 'event_extensions', 'jasny']
         }
     },
     waitSeconds: 2
