f2004ef85c15733c7ee71ecfbaba418ff8a60196
aeneasr <aeneas.rekkas@serlo.org>
2014-08-03 19:54:14 +0200

    Minor fixes

--- src/module/Entity/src/Entity/Provider/TokenProvider.php
+++ src/module/Entity/src/Entity/Provider/TokenProvider.php
@@ -48,7 +48,7 @@ class TokenProvider extends AbstractProvider implements ProviderInterface
                 $path[] = $term->getName();
                 $term = $term->getParent();
             }
-            array_reverse($path);
+            $path = array_reverse($path);
             $path = implode('/', $path);
         }
 
